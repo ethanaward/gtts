a47867c28f7035525d084f666972eb8671cda2e3
Ethan Ward <ethan.ward@mycroft.ai>
2017-07-14 16:24:40 -0500

    Add gtts 1.1.8 source and debian/

---
+++ setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+exec(open('gtts/version.py').read())
+
+setup(
+    name='gTTS',
+    version=__version__,
+    author='Pierre Nicolas Durette',
+    author_email='pndurette@gmail.com',
+    url='https://github.com/pndurette/gTTS',
+    packages=['gtts'],
+    scripts=['bin/gtts-cli', 'bin/gtts-cli.py'],
+    license='MIT',
+    description='Create an mp3 file from spoken text via the Google TTS (Text-to-Speech) API',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests",
+        "gtts_token"
+    ],
+    classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Multimedia :: Sound/Audio :: Speech'
+    ],
+)
